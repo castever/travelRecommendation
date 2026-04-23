0accd95feff2bddc1c51870f4825e5387da87666
castever <castever@gmail.com>
2026-04-23 16:51:56 -0500

    implement travel recommendation site

---
+++ travel_recommendation.js
@@ -0,0 +1,58 @@
+// get inputs and buttons
+const btnSearch = getElement("btnSearch");
+const btnClear = getElement("btnClear");
+
+const searchTermInput = getElement("searchInput");
+
+const introDiv = getElement("intro");
+const resultDiv = getElement("results");
+
+function performSearch() {
+  fetch("./travel_recommendation_api.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const searchTerm = searchTermInput.value.toLowerCase();
+
+      let results = [];
+
+      if (searchTerm.includes("beach")) {
+        results = data.beaches;
+      } else if (searchTerm.includes("templ")) {
+        results = data.temples;
+      } else if (searchTerm.includes("count")) {
+        results = data.countries[0].cities;
+      }
+
+      let html = "";
+      resultDiv.innerHTML = html;
+
+      for (const result of results) {
+        html += `<div class="card">
+<div><img src="${result.imageUrl}" alt="image of ${result.name}" /></div>
+
+<h3>${result.name}</h3>
+<p>${result.description}</p>
+
+</div>`;
+      }
+
+      resultDiv.innerHTML = html;
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+      resultDiv.innerHTML = "An error occurred while performing the search.";
+    });
+}
+
+function clearResults() {
+  resultDiv.innerHTML = "";
+  searchTermInput.value = "";
+}
+
+function getElement(id) {
+  return document.getElementById(id);
+}
+
+// set up event handling
+btnSearch.addEventListener("click", performSearch);
+btnClear.addEventListener("click", clearResults);
